d39067645aba1f37f3c5fae323b8435e7957db36
Tiago Figueira <tiago.g.figueira@gmail.com>
2022-10-28 16:54:21 -0300

    Funcoes - Argumentos e parametros

---
+++ Fundamentar/guia-estelar-javascript/funcoes/argumentos-e-parametros/argumentos-e-parametros.js
@@ -0,0 +1,9 @@
+// Function expression
+// Function anonymous
+
+// parâmetros (parameters)
+const sum = function(number1, number2) {
+    console.log(number1 + number2)
+}
+
+sum(2, 3) // Arguments - Argumentos
